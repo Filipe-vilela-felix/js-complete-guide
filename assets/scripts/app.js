46697e04ee3ab45a65b5be78d195040a3642eceb
Filipe Vilela <fvf0092000@gmail.com>
2023-09-17 16:09:46 -0300

    secao 2: literal de modelo e quebra linha

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,26 +3,14 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMessage = 'An erro \n' +
+                   'ocurred';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);
 
 /*
-VARIÁVEIS E CONSTANTES:
-    Uma variável é um contêiner para armazenar dados. 
-    Uma constante é semelhante a uma variável, exceto que não pode ser alterada depois de ser declarada. 
-    Embora a variável currentresult receba uma constante, na linha 4 ela é reatribuída atrávés de uma nova expressão.
-
-NOMEANDO VARIÁVEIS:
-    Permitido:
-        let userName -> camelCase (melhor prática);
-        let ageGroup5 -> Apenas letras e dígitos;
-        let $kindOfSpecial -> Começando com "$" é permitido;
-        let _intervalName -> Começando com "_" é permitido;
-    
-    Não permitido/Não recomendado:
-        let user_name -> Permitdo, mas não é uma boa prática;
-        let 21Players -> Começar com digitos não é permitido;
-        let user-b -> Sem caracteres especiais;
-        let let -> Palavras-chave não é permitido;
+LITERAL DE MODELO:
+    Ao invés da concatenação de strings usando o operador '+', podemos usar um literal de modelo, através da sintaxe ${...}, iserir variáveis ou expressões
+    diretamente de uma string.
 */
