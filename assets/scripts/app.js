9c6c6036d0ed8dde98acbd6d31d647d7d130b99f
Filipe Vilela <fvf0092000@gmail.com>
2023-09-18 10:00:42 -0300

    section 2: addEventListener and executing functions 'indirectly'

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,25 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2; // Calcula a soma;
-    return result;  // Retorna o resultado; 
-    alert('Hello World'); // Este alert não será apresentado na tela, pois o código depois de return não é executado;
+function add() {
+    currentResult = currentResult + userInput.value; 
+    outputResult(currentResult, '');
 }
 
-const additionResult = add(1, 2);   // Chama a função e armazena o resultado retornado;
-add(5, 5);
+addBtn.addEventListener('click', add); 
 
-currentResult = additionResult;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
 
 /*
-RETURN:
-    O return é necessário para que o resultado de soma, nesse caso, seja acessível fora da função.
+AddEventListener():
+    É uma função incorporada ao navegador, estando "ligada" ao botão.
+    O primeiro parâmetro/argumento é uma string, que identifica o elemento que queremos ouvir. 
+    O segundo, é uma função, pois queremos ter certeza de informar ao navegador, que informará ao js, qual função deve ser executada se o botão for cliclado.
+    Note que a função add não apresneta '()'. Isso porque queremos que a função só seja executada quando o botão for cliclado. E ao fazer isso, estamos
+    avisando ao mecanismo js: "Quando este botão for clicado, dê uma olhada na função com este nome e execute-a para mim".
+
+Note que a função add() não apresenta mais um return como no commit anterior. Isso porque com a função 'click', o navegador sabe que com apenas um
+clique, ele executa essa função, não se importando se há um retorno ou não.
+E também alterar o 'const result' pelo 'currentResult', para garantir que mudamos imediatamente o resultado atual. (linha 5);
+
+Obs: O código não está em pleno funcionamento. Segue os próximos commits;
 */
