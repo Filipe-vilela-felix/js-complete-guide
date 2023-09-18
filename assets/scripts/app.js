8295a4d822694a18bf5512a1a0e931bfd9e1ef63
Filipe Vilela <fvf0092000@gmail.com>
2023-09-18 17:51:58 -0300

    section 2: converting data types -> parseInt(), parseFloat(), .toString()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,24 +2,24 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value; 
+    currentResult = currentResult + parseInt(userInput.value);
     outputResult(currentResult, '');
 }
 
-addBtn.addEventListener('click', add); 
+addBtn.addEventListener('click', add);
 
 
 /*
-AddEventListener():
-    É uma função incorporada ao navegador, estando "ligada" ao botão.
-    O primeiro parâmetro/argumento é uma string, que identifica o elemento que queremos ouvir. 
-    O segundo, é uma função, pois queremos ter certeza de informar ao navegador, que informará ao js, qual função deve ser executada se o botão for cliclado.
-    Note que a função add não apresneta '()'. Isso porque queremos que a função só seja executada quando o botão for cliclado. E ao fazer isso, estamos
-    avisando ao mecanismo js: "Quando este botão for clicado, dê uma olhada na função com este nome e execute-a para mim".
+CONVERTENDO STRING EM NÚMERO:
+    Se essa linha...
+        currentResult = currentResult + userInput.value; 
+    ...ficar desse jeito com o intuito de incrementar o valor inicial '0' com o valor a ser inserido pelo usuário, ao invés de incrementar, 
+    o código fará uma concatenação. E isso acontece porque o javaScipt entende userInput.value como uma string e não como um número.
+    E para que isso seja corrigido...
+        currentResult = currentResult + parseInt(userInput.value); (linha 5);
 
-Note que a função add() não apresenta mais um return como no commit anterior. Isso porque com a função 'click', o navegador sabe que com apenas um
-clique, ele executa essa função, não se importando se há um retorno ou não.
-E também alterar o 'const result' pelo 'currentResult', para garantir que mudamos imediatamente o resultado atual. (linha 5);
-
-Obs: O código não está em pleno funcionamento. Segue os próximos commits;
+CONVERTENDO NÚMERO EM STRING:
+    Para converter um número em string, basta...
+        currentResult = currentResult.toString()
+    ... (o que seria a linha 5);
 */
