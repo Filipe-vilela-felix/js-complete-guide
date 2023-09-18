be69c877287c4b806146b3a9c996141bfcf90d25
Filipe Vilela <fvf0092000@gmail.com>
2023-09-18 18:39:40 -0300

    section 2: splitting code into funcitons -> making the sum functional

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
-
-
-/*
-CONVERTENDO STRING EM NÚMERO:
-    Se essa linha...
-        currentResult = currentResult + userInput.value; 
-    ...ficar desse jeito com o intuito de incrementar o valor inicial '0' com o valor a ser inserido pelo usuário, ao invés de incrementar, 
-    o código fará uma concatenação. E isso acontece porque o javaScipt entende userInput.value como uma string e não como um número.
-    E para que isso seja corrigido...
-        currentResult = currentResult + parseInt(userInput.value); (linha 5);
-
-CONVERTENDO NÚMERO EM STRING:
-    Para converter um número em string, basta...
-        currentResult = currentResult.toString()
-    ... (o que seria a linha 5);
-*/
