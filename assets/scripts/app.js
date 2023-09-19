1403cce47d64e8c1ba81633dd6c3e21a9c5ddcc7
Filipe Vilela <fvf0092000@gmail.com>
2023-09-19 16:04:33 -0300

    section 2: core data types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,66 +1,12 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
-
-// Gets input from input field
-function getUserNumberInput() {
-    return parseInt(userInput.value);
-}
-
-// Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription); // from vendor field
-}
-
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult += enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber);
-}
-
-function subtract() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult -= enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber);
-}
-
-function multiply() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult *= enteredNumber;
-    createAndWriteOutput('*', initialResult, enteredNumber);
-}
-
-function divide() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult /= enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber);
-}
-
-
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
-
 /*
-OPERADORES:
-    + -> soma dois números;
-    - -> subtrai dois números;
-    * -> multiplica dois números;
-    / -> divide dois números;    
-    % -> o resto da divisão de dois números;
-    ** -> exponenciação;s
+TIPOS DE DADOS:
+    Numbes -> Importante para cálculos e código onde você precisa "trabalhar com um número";
 
-OPERADORES ABREVIADOS:
-    = -> atribuir valor à variável;
-    +=, -=, ... -> executar cálculo e reatribuir resultado à variável;
-    ++, -- -> valor da variável incremento/decremento + reatribuição;
+    Strings(textos) -> Importante para a saída de resultados, coletando informações;
 
-    Ex: Ao invés de, por exemplo, currentResult = currentResult + enteredNumber, podemos fazer: currentResult += enterdNumber. 
-        Isso em todos os operadores. E também ao invés de currentResult = currentResult + 1, podemos fazer: currentResult++. 
- */
+    Booleans -> Importante para código condicional e situações em que você só tem 2 opções;
+
+    Objects -> Importante para dados agrupados/relacionados, ajuda você a organizar dados;
+    
+    Arrays -> Importante para dados de lista, desconhecido quantidades de dados;
+*/
