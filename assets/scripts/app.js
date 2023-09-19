b302e8d8e55ae48ff38593082d55b4612ff2e8e7
Filipe Vilela <fvf0092000@gmail.com>
2023-09-19 17:15:20 -0300

    section 2: using arrays

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,58 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets input from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor field
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries[1]);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 /*
-TIPOS DE DADOS:
-    Numbes -> Importante para cálculos e código onde você precisa "trabalhar com um número";
-
-    Strings(textos) -> Importante para a saída de resultados, coletando informações;
-
-    Booleans -> Importante para código condicional e situações em que você só tem 2 opções;
-
-    Objects -> Importante para dados agrupados/relacionados, ajuda você a organizar dados;
-    
-    Arrays -> Importante para dados de lista, desconhecido quantidades de dados;
-*/
+ARRAY:
+    - A função .push() empurra um novo elemento ao array, que por sua vez, adiciona um novo elemento a lista. E em seguida, expecificar o elemento
+      que deseja inserir na matriz. (linha 3 e 21);
+    - Para apossibilidade de gerenciar o envio de acesso de elementos. Na matriz é possível acesar seu índice. (linha 22); 
+ */
