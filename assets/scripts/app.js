cca73a8a569af9036f5a9bda254f0c2c28213c54
Filipe Vilela <fvf0092000@gmail.com>
2023-09-19 15:51:22 -0300

    section 2: more operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,28 +15,28 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
@@ -46,3 +46,21 @@ addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
+
+/*
+OPERADORES:
+    + -> soma dois números;
+    - -> subtrai dois números;
+    * -> multiplica dois números;
+    / -> divide dois números;    
+    % -> o resto da divisão de dois números;
+    ** -> exponenciação;s
+
+OPERADORES ABREVIADOS:
+    = -> atribuir valor à variável;
+    +=, -=, ... -> executar cálculo e reatribuir resultado à variável;
+    ++, -- -> valor da variável incremento/decremento + reatribuição;
+
+    Ex: Ao invés de, por exemplo, currentResult = currentResult + enteredNumber, podemos fazer: currentResult += enterdNumber. 
+        Isso em todos os operadores. E também ao invés de currentResult = currentResult + 1, podemos fazer: currentResult++. 
+ */
