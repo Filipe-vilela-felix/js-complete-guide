746fb3d178101a13bce7e392662401e4e2b84e94
Filipe Vilela <fvf0092000@gmail.com>
2023-09-20 15:14:57 -0300

    section 2: the 'typeof' operator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,77 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets input from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor field
+}
+
+function writeToLog(
+    operationIdentifier, 
+    prevResult, 
+    operationNumber, 
+    newResult) {
+        const logEntry = {
+            operation: operationIdentifier,
+            prevResult: prevResult,
+            number: operationNumber,
+            result: newResult
+        };
+        logEntries.push(logEntry);
+        console.log(logEntry.operation);
+        console.log(logEntries);
+        console.log(typeof logEntry);
+    }
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult); 
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 /*
-UNDEFINED, NULL & NaN:
-    Undefined (indefinido): Valor padrão de variáveis não inicializadas.            
-                            Você não deve atribuir undefined como um valor manualmente.
-    Null:  Nunca assumido por inadimplência.
-           Você pode atribuir isso é um valor se você quiser "reset" / "clear" uma variável.
-    NaN: Tecnicamente, é do tipo número e pode portanto, ser usado em Cálculos. 
-         Produz um novo NaN e é o resultado de cálculos inválidos (por exemplo, 3 * 'oi').
+TYPEOF:
+    Essa palavra chave typeof permite avaliar o tipo de uma variável em tempo de execução.
+    Ao abrir o console do navegador e clilcar no botão '+', fazendo a leitura da função 'add', notará que aparecerá logo abixo a palavra 'object'. 
+    Fazendo jus ao typeof. (linha 30);
 */
