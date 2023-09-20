db144189857ad9e7799682c08a84ebf18d47c6fb
Filipe Vilela <fvf0092000@gmail.com>
2023-09-20 15:02:41 -0300

    section 2: undefined, null e NaN

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,76 +1,9 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
-let logEntries = [];
-
-// Gets input from input field
-function getUserNumberInput() {
-    return parseInt(userInput.value);
-}
-
-// Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription); // from vendor field
-}
-
-function writeToLog(
-    operationIdentifier, 
-    prevResult, 
-    operationNumber, 
-    newResult) {
-        const logEntry = {
-            operation: operationIdentifier,
-            prevResult: prevResult,
-            number: operationNumber,
-            result: newResult
-        };
-        logEntries.push(logEntry);
-        console.log(logEntry.operation);
-        console.log(logEntries);
-    }
-
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult += enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber);
-    writeToLog('ADD', initialResult, enteredNumber, currentResult);
-}
-
-function subtract() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult -= enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber);
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult); 
-}
-
-function multiply() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult *= enteredNumber;
-    createAndWriteOutput('*', initialResult, enteredNumber);
-    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
-}
-
-function divide() {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult /= enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber);
-    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
-}
-
-
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
-
 /*
-CRIANDO UM FUNÇÃO REUTILIZÁVEL QUE SEJA UM OBJETO:
-    Para que o objeto de nome logEntry não seja muito repetitiva e redundante em cada função que compoe a calculadora (add(), subtract(), multiply() e divide())
-        criamos uma função que armazenasse tal objeto, onde em cada função que compoe os calculos, possa incluir suas respectivas propriedades/métodos.
-        (linha 16 a 30, 37, 45, 53 e 61);
+UNDEFINED, NULL & NaN:
+    Undefined (indefinido): Valor padrão de variáveis não inicializadas.            
+                            Você não deve atribuir undefined como um valor manualmente.
+    Null:  Nunca assumido por inadimplência.
+           Você pode atribuir isso é um valor se você quiser "reset" / "clear" uma variável.
+    NaN: Tecnicamente, é do tipo número e pode portanto, ser usado em Cálculos. 
+         Produz um novo NaN e é o resultado de cálculos inválidos (por exemplo, 3 * 'oi').
 */
