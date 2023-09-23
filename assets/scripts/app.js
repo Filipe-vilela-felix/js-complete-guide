8ce0c1c9bae54319c35eb4da8f9dcbc7dce20dbf
Filipe Vilela <fvf0092000@gmail.com>
2023-09-23 19:03:57 -0300

    section 4: using 'if' statements for checking the win-condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATACK_VALUE = 10;
+const MONSTER_ATACK_VALUE = 14;
 
-let chosenMaxLife = 50;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
@@ -9,29 +10,21 @@ adjustHealthBars(chosenMaxLife);
 function atackHandler() {
     const damage = dealMonsterDamage(ATACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if(currentMonsterHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You won');
+    } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost');
+    } else if(currentMonsterHealth <= 0 && currentPlayerHealth <=0) {
+        alert('You have a draw');
+    }
 }
 
 attackBtn.addEventListener('click', atackHandler);
 
 /*
 CONTEXTUALIZANDO O CÓDIGO:
-    - Primiero, declaramos uma constante chamada ATACK_VALUE e atribuindo a ela o valor 10. Isso representa a quantidade de dano que um ataque irá causar. (linha 1);
-    
-    - Declaramos uma variável chamada choseMaxLife, e atribuimos a ela o valor de 50. (linha 3);
-    
-    - Declaramos uma variável chamada currentMonsterHealth, e atribuimos a ela o valo de choseMaxLife. Representando a quantidade atual de vida que o mostro tem. (linha 4);
-    
-    - Chamamos uma função chamada adjustHealthBars e passando para ela o valor de chosenMaxLife. Essa função ajusta as barras de vida na interface do 
-        usuário para refletir a quantidade máxima de vida. (linha 7);
-    
-    - A função atackHandler() é declarada. Esta função será executada quando o jogador atacar. (linha 9);
-    
-    - Dentro da função atackHandler(), a constante damage é declarada e atribuída ao valor retornado pela função dealMonsterDamage(ATACK_VALUE). 
-        A função dealMonsterDamage() provavelmente calcula a quantidade de dano que um ataque causa ao monstro. (linha 10);
-    
-    - Ainda dentro da função atackHandler(), o valor de currentMonsterHealth é reduzido pelo valor de damage. 
-        Isso simula o monstro recebendo dano do ataque. (linha 11);
-    
-    - Finalmente, um ouvinte de evento é adicionado ao botão de ataque. Quando o botão de ataque é clicado, a função atackHandler() é chamada, 
-        fazendo com que o jogador ataque o monstro. (linha 14);
+    Além do desenvolvimento presente no commit anterior, onde adicionanmos uma funcionalidade de ataque ao "monstro", nesse adicionamos uma funcionalidade
+     onde recebemos um dano aleatório a partir do momento em que atacamos. (linha 2, 13 a 21);
 */
