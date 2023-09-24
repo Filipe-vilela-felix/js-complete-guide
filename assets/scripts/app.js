40ba430de929608ff8d605a0136f97380d9484c3
Filipe Vilela <fvf0092000@gmail.com>
2023-09-24 10:47:42 -0300

    section 4: adding more 'if' statements and a 'strong attack' funcionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,24 +8,42 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function atackHandler() {
-    const damage = dealMonsterDamage(ATACK_VALUE);
+function monsterAttack(mode) {
+    let maxDamage;
+    if(mode === 'ATTACK') {
+        maxDamage = ATACK_VALUE;
+    } else if(mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATACK_VALUE);
     currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth <= 0 && currentMonsterHealth > 0) {
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won');
-    } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    } else if(currentPlayerHealth <= 0 && currentPlayerHealth > 0) {
         alert('You lost');
     } else if(currentMonsterHealth <= 0 && currentPlayerHealth <=0) {
         alert('You have a draw');
     }
 }
 
-attackBtn.addEventListener('click', atackHandler);
+function attackHandler() {
+    monsterAttack('ATTACK');
+}
+
+function strongAttackHandler() {
+    monsterAttack('STRONG_ATTACK');
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
 
 /*
 CONTEXTUALIZANDO O CÓDIGO:
-    Além do desenvolvimento presente no commit anterior, onde adicionanmos uma funcionalidade de ataque ao "monstro", nesse adicionamos uma funcionalidade
-     onde recebemos um dano aleatório a partir do momento em que atacamos. (linha 2, 13 a 21);
+    A diferença do commit anterior para essse, é que ao criarmos a função de "ataque forte", o código ficou muito redundante, pois era só alterar o valor
+        de uma variável e pronto. Portanto, ao invés de repetirmos tantas linhas de código, cogitamos em criar uma função que pudesse ser reutilizavel 
+        tanto na função de ataque quanto na de ataque forte.
+    Para isso, foi necessário criarmos uma variável e um parâmetro que de acordo com o tipo de ataque, mudasse seu valor, e assim resultando na mudança 
+        de todo o código. (linhas 2, 11 a 18, 31 a 40);
 */
