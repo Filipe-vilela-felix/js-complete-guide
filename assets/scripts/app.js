8b85654c1bb32f89bf37eea61e8b1a2404d3bcbc
Filipe Vilela <fvf0092000@gmail.com>
2023-09-26 15:42:00 -0300

    section 4: controlling the conditional bonus life

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,57 +1,68 @@
-const ATACK_VALUE = 10;
+const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
-const MONSTER_ATACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
-    const playerDamage = dealPlayerDamage(MONSTER_ATACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You won');
-    } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You lost');
-    } else if(currentMonsterHealth <= 0 && currentPlayerHealth <=0) {
-        alert('You have a draw');
-    }
+  const initialPlayerHealth = currentPlayerHealth;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('You would be dead but the bonus life saved you!');
+  }
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
 }
 
-function monsterAttack(mode) {
-    let maxDamage;
-    if(mode === 'ATTACK') {
-        maxDamage = ATACK_VALUE;
-    } else if(mode === 'STRONG_ATTACK') {
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= damage;
-    endRound();
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
 }
 
 function attackHandler() {
-    monsterAttack('ATTACK');
+  attackMonster('ATTACK');
 }
 
 function strongAttackHandler() {
-    monsterAttack('STRONG_ATTACK');
+  attackMonster('STRONG_ATTACK');
 }
 
 function healPlayerHandler() {
-    let healValue;
-    if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
-        alert("You can't heal to more than your max intial health");
-        healValue = chosenMaxLife - currentMonsterHealth;
-    } else {
-        healValue = HEAL_VALUE;
-    }
-    increasePlayerHealth(healValue);
-    currentPlayerHealth += healValue;
-    endRound();
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than your max initial health.");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
+  endRound();
 }
 
 attackBtn.addEventListener('click', attackHandler);
@@ -60,17 +71,9 @@ healBtn.addEventListener('click', healPlayerHandler);
 
 /*
 CONTEXTUALIZANDO O CÓDIGO:
-    A diferença do commit anterior para essse, é que ao criarmos a função de cura. 
-    
-    - Criamos uma variável que mantém o valor de cura fixo. (linha 4);
-    
-    - A função endRound() é definida. Ela lida com o final de cada rodada, calculando o dano ao jogador, atualizando a saúde do jogador e 
-        verificando as condições de vitória ou derrota. (linhas 12 a 22);
-    
-    - A função monsterAttack(mode) é modificada para chamar a função endRound() após causar dano ao monstro. (linha 54);
-    
-    - A função healPlayerHandler() é definida. Ela permite ao jogador curar durante o jogo, verificando se a saúde do jogador excederá a saúde máxima 
-        inicial após a cura e ajustando o valor de cura de acordo. Em seguida, ela aumenta a saúde do jogador e chama a função endRound(). (linhas 44 a 55);
-    
-    - Um novo EventListener é adicionado ao botão de cura para chamar a função healPlayerHandler() quando o botão é clicado. (linha 69);
+    A diferença do commit anterior para essse:
+
+    - Uma nova variável "hasBonusLife" foi adicionada e inicializada com valor true. Usado para determinar se o jogador tem uma vida extra. (linha 9);
+    - A função endRound() foi modificada para incluir uma verificação de hasBonusLife. Se a saúde do jogador for 0 ou menos e hasBonusLife for verdadeiro, 
+        a função remove a vida extra do jogador, restaura a saúde do jogador para o valor inicial da rodada e exibe um alerta informando que a vida extra salvou o jogador.
 */
