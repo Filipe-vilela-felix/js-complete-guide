6e52ea92b2c099ac4fcd35c6d87c0ad65c91f563
Filipe Vilela <fvf0092000@gmail.com>
2023-10-08 11:03:25 -0300

    section 6: anonymous functions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function() {
-    console.log('Game is starting...');
-};
+// const start = function() {
+//     console.log('Game is starting...');
+// };
 
 // const person = {
 //     name: 'Max',
@@ -15,15 +15,26 @@ const start = function() {
 
 // console.dir(startGame);
 
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function() {
+    console.log('Game is starting...');
+});
 
 /*
-    Neste commit aprendemos que é possível armazenar funções em variáveis. 
-    E quando isso é feito, deixa de ser uma declaração de função e passa a ser uma expressão de função.
-    E quando uma função passa a ser uma expressão, não precisamos de dar um nome expecífico para a função, 
-        deixando apenas como function().... (linha 3);
-    
-    Por fim, o ponto e vírgula no final da função é necessário porque estamos fazendo uma atribuição - 
-        estamos atribuindo uma função anônima à variável 'start'. (linha 5);
+FUNÇÕES ANÔNIMAS:
+    - Funções anônimas são funções sem nome.
 
+    - Podemos armazenar funções anônimas em uma variável e usar o nome da variável para chamar a função. (linhas comentadas 3 a 5);
+
+    - Funções anônimas são útei quando não preciso usa-las em outro lugar do código.
+
+    - Um exemplo comum de seu uso, é quando adicionamos um ouvinte de evento. 
+        Podemos definir a função diretamente no local onde ela é necessária. (linha 18 a 20);
+
+    - Se a função anônima for usada apenas uma vez e não for necessária em nenhum outro lugar do código, 
+        usar função anônima pode economizar umas linhas de código e torna-lo mais fácil de ler.
+
+    - No entanto, se a função anônima gerar um error, pode ser útil dar um nome a função para fins de depuração. Em outras palavras, 
+        podemos usar uma função declarada.
+
+    - Contudo, se vc sabe que seu código está correto e não está gerando erro, pode não ser necessário dar um nome a sua função anônima. (linha 18);
 */
