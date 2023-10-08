7fe7b4b03d6383d6ea0756ccd5e83015c577c436
Filipe Vilela <fvf0092000@gmail.com>
2023-10-08 12:32:06 -0300

    section 6: 'adding user choices to the game'

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function() {
-//     console.log('Game is starting...');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSOR';
+const DEFAULT_USER_CHOICE = ROCK;
 
-// const person = {
-//     name: 'Max',
-//     greet: function greet() {
-//         console.log('Hello there!');
-//     }
-// }
+let gameIsRunnig = false;
 
-// person.greet();
-
-// console.dir(startGame);
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (
+        selection !== ROCK && 
+        selection !== PAPER && 
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We choice ${DEFAULT_USER_CHOICE} for you!`);    
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+}
 
 startGameBtn.addEventListener('click', function() {
+    if (gameIsRunnig) {
+        return;
+    }
     console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
 
 /*
-FUNÇÕES ANÔNIMAS:
-    - Funções anônimas são funções sem nome.
+CONTEXTUALIZANDO O CÓDIGOA:
+    - Criamos a função getPlayerChoice:
+        Esta função solicita ao usuário que faça uma escolha entre pedra, papel ou tesoura. 
+        Se a escolha do usuário não for válida (ou seja, não for nem pedra, nem papel, nem tesoura),
+        a função alerta o usuário de que sua escolha foi inválida e define a escolha como ‘Pedra’ por padrão. (linhas 10 a 21);
+    
+    - Em seguida definimos algumas constantes globais:
+        ROCK, PAPER, SCISSORS e DEFAULT_USER_CHOICE. Essas constantes representam as possíveis escolhas no jogo. (linhas 3 a 6);
+    
+    - Definimos uma variável global gameIsRunning e a inicializa como false. 
+        Esta variável é usada para verificar se o jogo já está em execução. (linha 8);
 
-    - Podemos armazenar funções anônimas em uma variável e usar o nome da variável para chamar a função. (linhas comentadas 3 a 5);
+    - Por fim, o adicionamos um ouvinte de evento ao botão ‘startGameBtn’.
+        Quando o botão é clicado, a função anônima dentro do ouvinte de evento é executada. Esta função verifica se o jogo já está em execução. 
+        Se não estiver, define gameIsRunning como true, imprime uma mensagem no console indicando que o jogo está começando e 
+        chama a função getPlayerChoice para obter a escolha do jogador. (linhas 23 a 30);
 
-    - Funções anônimas são útei quando não preciso usa-las em outro lugar do código.
-
-    - Um exemplo comum de seu uso, é quando adicionamos um ouvinte de evento. 
-        Podemos definir a função diretamente no local onde ela é necessária. (linha 18 a 20);
-
-    - Se a função anônima for usada apenas uma vez e não for necessária em nenhum outro lugar do código, 
-        usar função anônima pode economizar umas linhas de código e torna-lo mais fácil de ler.
-
-    - No entanto, se a função anônima gerar um error, pode ser útil dar um nome a função para fins de depuração. Em outras palavras, 
-        podemos usar uma função declarada.
-
-    - Contudo, se vc sabe que seu código está correto e não está gerando erro, pode não ser necessário dar um nome a sua função anônima. (linha 18);
+OBSERVAÇÃO:
+    JavaScript permite que você acesse propriedades e métodos (como toUpperCase() para strings) em valores primitivos como se fossem objetos. (linha 11);
+        Isso acontece porque, quando você tenta acessar uma propriedade ou método em um valor primitivo, 
+        JavaScript cria temporariamente um objeto wrapper para esse valor primitivo e então acessa a propriedade ou método nesse objeto wrapper. 
+        Isso é feito apenas temporariamente e o valor primitivo original permanece inalterado.
 */
