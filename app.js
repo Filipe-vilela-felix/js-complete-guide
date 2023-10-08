ec5db2da6fe62a97f5dcf3800760e419722c5fcf
Filipe Vilela <fvf0092000@gmail.com>
2023-10-08 15:26:36 -0300

    section 6: introducing arrow functions

--- app.js
+++ app.js
@@ -1,76 +1,106 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSOR';
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSOR";
 const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW = 'DRAW';
-const RESULT_PLAYER_WINS = 'PLAYER_WINS';
-const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunnig = false;
 
-const getPlayerChoice = function() {
-    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
-    if (
-        selection !== ROCK && 
-        selection !== PAPER && 
-        selection !== SCISSORS
-    ) {
-        alert(`Invalid choice! We choice ${DEFAULT_USER_CHOICE} for you!`);    
-        return DEFAULT_USER_CHOICE;
-    }
-    return selection;
-}
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We choice ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
 
-const getComputerChoice = function() {
-    const ramdomValue = Math.random();
-    if (ramdomValue < 0.34) {
-        return ROCK;
-    } else if (ramdomValue < 0.67) {
-        return PAPER;
-    } else {
-        return SCISSORS;
-    }
-}
+const getComputerChoice = () => {
+  const ramdomValue = Math.random();
+  if (ramdomValue < 0.34) {
+    return ROCK;
+  } else if (ramdomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
 
-const getWinner = function(cChoice, pChoice) {
-    if (cChoice === pChoice) {
-        return RESULT_DRAW;
-    } else if (
-        (cChoice === ROCK && pChoice === PAPER) || 
-        (cChoice === PAPER && pChoice === SCISSORS) ||
-        (cChoice === SCISSORS && pChoice === ROCK)
-    ) {
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    }
-}
+// const add = (a, b) => a + b;
+// const add2 = function(a, b) {
+//     return a + b;
+// }
 
-startGameBtn.addEventListener('click', function() {
-    if (gameIsRunnig) {
-        return;
-    }
-    console.log('Game is starting...');
-    const PlayerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice, PlayerChoice);
-    console.log(winner);
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+// if (cChoice === pChoice) {
+//     return RESULT_DRAW;
+// } else if (
+//     (cChoice === ROCK && pChoice === PAPER) ||
+//     (cChoice === PAPER && pChoice === SCISSORS) ||
+//     (cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+//     return RESULT_PLAYER_WINS;
+// } else {
+//     return RESULT_COMPUTER_WINS;
+// }
+
+startGameBtn.addEventListener("click", () => {
+  if (gameIsRunnig) {
+    return;
+  }
+  console.log("Game is starting...");
+  const PlayerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, PlayerChoice);
+  console.log(winner);
 });
 
 /*
-CONTEXTUALIZANDO O CÓDIGOA:
-    - Adicionamos a função getComputerChoice() para calcular a escolha do computador. Esta função gera um número aleatório entre 0 e 1. 
-        Se o número estiver entre 0 e 0.33, a escolha do computador é ROCK. 
-        Se estiver entre 0.34 e 0.67, a escolha é PAPER. Para qualquer número acima de 0.67, a escolha é SCISSORS. (linhas 26 a 35);
+FUNÇÃO DE SETA:
+    Sempre que tivermos uma função anônima, podemos usar uma sintaxe diferente. 
+    Nessa sintaxe, retiramos a nomenclatura "function" e adicionamos após os parênteses uma seta '=>'. Isso se chama FUNÇÃO DE SETA.
 
-    - O código adiciona outra função chamada getWinner para determinar o vencedor do jogo. 
-        Esta função recebe a escolha do computador e a escolha do jogador como argumentos. (linha 37);
+    Sintaxe geral: 
+        (arg1, arg2) => {...}
+        
 
-    - A função getWinner primeiro verifica se a escolha do computador é igual à escolha do jogador. 
-        Se forem iguais, então é um empate e a função retorna RESULT_DRAW. Se não for um empate, a função verifica se o jogador ganhou. (linhas 38 a 48);
+    Sem argumentos / Parâmetros: 
+        () => {...}
+    
+    Exatamente (1) argumento / Parâmetro: 
+        arg => {...}
 
-    - Finalmente, o código chama a função getWinner com as escolhas do computador e do jogador como argumentos e 
-        armazena o resultado em uma constante chamada winner. O vencedor é então registrado no console. (linhas 58 e 59);
+    Exatamente uma expressão no corpo da função: 
+        (a, b) => a + b
+    
+    Mais de uma expressão no corpo da função: 
+        (a, b) => { 
+            a *= 2; 
+            return a + b; 
+        }
+
+    A vantagem de usa-la é que ela apresenta alguns truques extras:
+        Para demonstrar uma das vantagens, ao invés de deixarmos o corpo da função getWinner() como está (linhas comentadas 50 a 60), 
+            vamos substitui-lo por uma condicional ternária. (linhas 42 a 48);
+        Agora, como temos uma função de seta, onde há apenas uma expressão... 
+            podemos omitir as chaves, o ';' no final da função, e omitir a palavra chave return.
+        Para melhor entendimento, veja o exemplo comentado nas linha 36 a 39;
+
+CONTEXTUALIZANDO O CÓDIGO:
+    Alteramos todas as funções para funções de seta.
 */
