a80ad54eb89fbb95e9f252455162985d0a7097f8
Filipe Vilela <fvf0092000@gmail.com>
2023-10-09 18:59:20 -0300

    section 6: callback functions

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ startGameBtn.addEventListener("click", () => {
 
 // NÃO REÇLACIONADO AO JOGO:
 
-const sumUp = (a, b, ...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   }
@@ -98,7 +98,7 @@ const sumUp = (a, b, ...numbers) => {
   for (const num of numbers) {
     sum += validateNumber(num);
   }
-  return sum;
+  resultHandler(sum);
 };
 
 const subtractUp = function() {
@@ -109,17 +109,45 @@ const subtractUp = function() {
   return sum;
 };
 
-console.log(sumUp(1, 5, 'fdsa', 10, -3, 6, 10));
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+}
+
+sumUp(showResult, 1, 5, 'fdsa', 10, -3, 6, 10);
 console.log(subtractUp(1, 10, 15, 20));
 
 /*
-  O que difere este código do commit anterior, é o aprendizado que nos permite inserir uma função dentro de uma função. (linhas 93 a 95);
+  Ao examinarmos novamente a função addEventListnner(), podemos notar que como segundo argumento, criamos uma função anônima. (linha 62);
+  Uma função de callback é uma função que passamos para outra função como argumento, e então, 
+    a função "externa" chama a função de callback em algum momento.
+  As funções de callback são úteis quando temos algum código que precisa ser executado após algo acontecer, 
+    mas não sabemos exatamente como o código será. Ao usar uma função de callback, podemos tornar o código mais flexível e reutilizável.
 
 CONTEXTUALIZANDO O CÓDIGO:
-  A função sumUp() é uma função externa que aceita um número indeterminado de argumentos (através do operador de descanso). (linha 92); 
-  Dentro dessa função sumUp(), temos outra função chamada validateNumber(). Essa é a nossa função aninhada. (linhas 93 a 95);
-  
-  A função aninhada validateNumber() é usada para verificar se o valor fornecido é um número. 
-  Se o valor não for um número, ele retorna 0; caso contrário, retorna o próprio número. 
-  Essa função aninhada é usada para garantir que estamos somando apenas valores numéricos.
+  - A função sumUp() agora aceita um argumento adicional chamado resultHandler, que é esperado ser uma função 
+      (isso é o que chamamos de função de callback). (linha 92);
+    
+  - A função sumUp() ainda soma todos os números fornecidos (validando-os primeiro), mas em vez de retornar a soma diretamente, 
+      ela chama a função resultHandler e passa a soma para ela. (linha 101);
+
+  - A função showResult() é a função de callback que você passa para sumUp(). 
+      Ela recebe um resultado (neste caso, a soma dos números) e exibe um alerta com esse resultado. (linhas 112 a 113);
+
+  - Quando você chama sumUp(), agora você passa a função showResult() como o primeiro argumento, seguido pelos números que deseja somar. 
+      A função sumUp então faz sua coisa, somando os números, e quando termina, chama showResult com a soma.
+
+SIMPLIFICANDO PARA ESTUDO:
+  function sumUp(callback, ...numbers) {
+    let sum = 0;
+    for (let num of numbers) {
+      sum += num;
+    }
+    callback(sum);
+  }
+
+  function showResult(result) {
+    console.log('O resultado é: ' + result);
+  }
+
+  sumUp(showResult, 1, 2, 3, 4);
 */
