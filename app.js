1aa0a1b5d42c7eaa3d3a127f8b55164459181987
Filipe Vilela <fvf0092000@gmail.com>
2023-10-10 05:19:04 -0300

    section 6: working with 'bind()'

--- app.js
+++ app.js
@@ -89,65 +89,50 @@ startGameBtn.addEventListener("click", () => {
 
 // NÃO REÇLACIONADO AO JOGO:
 
-const sumUp = (resultHandler, ...numbers) => {
+const combine = (resultHandler, operation, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   }
 
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function() {
-  let sum = 0;
-  for (const num of arguments) {  // Não use isso!
-    sum -= num;
-  }
-  return sum;
-};
+// const subtractUp = function(resultHandler, ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {  // Não use isso!
+//     sum -= num;
+//   }
+//   resultHandler(sum);
+// };
 
-const showResult = (result) => {
-  alert('The result after adding all numbers is: ' + result);
+const showResult = (messageText, result) => {
+  alert(messageText + result);
 }
 
-sumUp(showResult, 1, 5, 'fdsa', 10, -3, 6, 10);
-console.log(subtractUp(1, 10, 15, 20));
+combine(showResult.bind(this, 'The result after adding all numbers is: '), 'ADD', 1, 5, 'fdsa', 10, -3, 6, 10);
+combine(showResult.bind(this, 'The result after subtracting all numbers is: '), 'SUBTRACT', 1, 10, 15, 20);
 
 /*
-  Ao examinarmos novamente a função addEventListnner(), podemos notar que como segundo argumento, criamos uma função anônima. (linha 62);
-  Uma função de callback é uma função que passamos para outra função como argumento, e então, 
-    a função "externa" chama a função de callback em algum momento.
-  As funções de callback são úteis quando temos algum código que precisa ser executado após algo acontecer, 
-    mas não sabemos exatamente como o código será. Ao usar uma função de callback, podemos tornar o código mais flexível e reutilizável.
+  O método .bind() é usado para criar uma nova função que tem seu contexto this definido para o valor fornecido, 
+    com uma sequência de argumentos precedendo quaisquer outros que sejam fornecidos quando a nova função é chamada.
+  Em outras palavras, ele é usado quando você quer que uma função seja chamada mais tarde, 
+    mas você quer que ela seja chamada com um contexto específico ou com alguns argumentos já definidos.
 
 CONTEXTUALIZANDO O CÓDIGO:
-  - A função sumUp() agora aceita um argumento adicional chamado resultHandler, que é esperado ser uma função 
-      (isso é o que chamamos de função de callback). (linha 92);
-    
-  - A função sumUp() ainda soma todos os números fornecidos (validando-os primeiro), mas em vez de retornar a soma diretamente, 
-      ela chama a função resultHandler e passa a soma para ela. (linha 101);
+  No commit anterior, você tinha duas funções separadas para somar e subtrair números (sumUp e subtractUp). 
+  Cada uma dessas funções aceitava um número variável de argumentos, somava ou subtraía todos os números e 
+    depois chamava uma função de callback com o resultado.
 
-  - A função showResult() é a função de callback que você passa para sumUp(). 
-      Ela recebe um resultado (neste caso, a soma dos números) e exibe um alerta com esse resultado. (linhas 112 a 113);
-
-  - Quando você chama sumUp(), agora você passa a função showResult() como o primeiro argumento, seguido pelos números que deseja somar. 
-      A função sumUp então faz sua coisa, somando os números, e quando termina, chama showResult com a soma.
-
-SIMPLIFICANDO PARA ESTUDO:
-  function sumUp(callback, ...numbers) {
-    let sum = 0;
-    for (let num of numbers) {
-      sum += num;
-    }
-    callback(sum);
-  }
-
-  function showResult(result) {
-    console.log('O resultado é: ' + result);
-  }
-
-  sumUp(showResult, 1, 2, 3, 4);
+  No commit atual, você tem apenas uma função (combine) que pode tanto somar quanto subtrair números. (linhas 92, 99 a 103);
+  Ela aceita um argumento adicional (operation) que determina se ela deve somar ou subtrair os números. (linhas 92 e 99);
+  Além disso, em vez de chamar diretamente a função de callback com o resultado, 
+    ela agora chama uma versão “ligada” da função de callback que inclui uma mensagem de texto.
 */
