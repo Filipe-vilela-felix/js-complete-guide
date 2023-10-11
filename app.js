bfc8d71a51bf8f7eb2e991c68ff3e3df6618ef47
Filipe Vilela <fvf0092000@gmail.com>
2023-10-11 18:22:11 -0300

    section 7: node & elements

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 /*
-  - O DOM é uma representação do código HTML de uma página web que foi carregada, e renderizada pelo navegador.
+  - Tudo no DOM é um nó. As tags HTML são nós de elemento.
 
-  - O JavaScript pode interagir com o DOM através de vários étodos e funcionalidades que o navegador expõe, como document.querySelector().
+  - Além dos nós de elemento, existem outros tipos de nós, como nós de texto.
 
-  - Esses métodos permitem o JavaScript acessar e manipular elementos HTML da página.
+  - Os elementos são nós que são criados a partir de tags HTML renderizadas.
 
-  - O DOM não está restrito aos navegadores. Outras ferramentas e linguagens (como Pyton ou Node.js) também podem analisar e manipular HTML.
+  - Em nós de elemento, você tem propriedades e métodos especiais para interagir com os elementos, 
+      alterar seu estilo, alterar seu conteúdo e assim por diante.
 
-  - Além do objeto document, que é o nó raiz do DOM, existe outro objeto global importante: o objeto window.
+  - Você normalmente trabalha mais com elementos do que com nós de texto porque geralmente deseja adicionar um novo elemento HTML, 
+      remover um, alterar o estilo de um, etc.
+  - As propriedades e métodos exatos disponíveis em cada elemento dependem do tipo de elemento. 
+      Por exemplo, em uma entrada onde o usuário pode inserir conteúdo, você tem maneiras de ler essa entrada.
 
-  - O objeto window representa a janela ou aba ativa do navegador e é o ponto de entrada global para o seu script.
+  - Você pode selecionar elementos, criar e removê-los com a ajuda de Javascript.
+  
+  - Os nós de texto são na maioria das vezes apenas criados automaticamente quando você configura o conteúdo de texto de um elemento para 
+      um novo conteúdo de texto.
 
-  - O objeto document é na verdade uma propriedade o objeto window.
+NODE VS ELEMENTO:
+  Um nó é uma entidade abstrata que pode ser um elemento, um atributo, um texto ou qualquer outro tipo de objeto que possa ser representado no DOM.
 
-  - O objeto window fornece acesso a todos os recursos que o navegador deseja expor para você, 
-      além de algumas propriedades específicas da janela, como a largura da janela.
+  Um elemento, por outro lado, é um tipo específico de nó que representa uma tag HTML no documento. 
+  Ele pode conter outros nós, como nós de texto ou até mesmo outros nós de elemento, formando uma estrutura de árvore.
+
+  Portanto, todos os elementos são nós, mas nem todos os nós são elementos. 
+  Por exemplo, um nó de texto (que contém texto dentro de uma tag HTML) é um nó, mas não é um elemento.
 */
