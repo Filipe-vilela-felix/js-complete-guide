79e682c2d8d4f4e6aaa441f3f6508f0443c208a3
Filipe Vilela <fvf0092000@gmail.com>
2023-10-17 15:55:28 -0300

    section 7: 'opening a modal by changing css classes'

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,11 @@ const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('button').lastElementChild;
+
+const backdrop = document.getElementById('backdrop');
+
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle('visible');  // Se a classe "visible" já estiver em existência quando a for lida, não é executada. Caso contrário, é.
+}
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
