6cc24ac3f196eab0a03f40c3682ae8676f920f42
Filipe Vilela <fvf0092000@gmail.com>
2023-10-17 23:55:44 -0300

    section 7: 'creating a movie in javascript & clearing the input'

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,81 +1,97 @@
-const addMovieModal = document.getElementById('add-modal');
+const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
-const startAddMovieButton = document.querySelector('header button');
+const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector('button').lastElementChild;
-const backdrop = document.getElementById('backdrop');
+const backdrop = document.getElementById("backdrop");
 // const background = document.body.childre[0];
 // const background = document.body.firstElementChild;
-const cancelAddMovieButton = addMovieModal.querySelector('.btn.btn--passive');
+const cancelAddMovieButton = addMovieModal.querySelector(".btn.btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-const userInputs = addMovieModal.querySelectorAll('input');
+const userInputs = addMovieModal.querySelectorAll("input");
 // const userInputs = addMovieModal.getElementsByTagName('input');
 
+const movies = [];
+
 const toggleBackdrop = () => {
-    backdrop.classList.toggle('visible');
+  backdrop.classList.toggle("visible");
 };
 
 const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible'); 
-    toggleBackdrop();
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const clearMovieModal = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
 };
 
 const cancelAddMovieHandler = () => {
-    toggleMovieModal();
+  toggleMovieModal();
+  clearMovieModal();
 };
 
 const addMovieHandler = () => {
-    const titleValue = userInputs[0].value;
-    const imageUrlValue = userInputs[1].value;
-    const ratingValue = userInputs[2].value;
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
 
-    if (
-        titleValue.trim() === '' || 
-        imageUrlValue.trim() === '' || 
-        ratingValue.trim() === '' || 
-        +ratingValue < 1 || 
-        +ratingValue > 5) {
-            alert('Please enter valid values (rating between 1 and 5)');
-            return;
-        }
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5)");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieModal();
 };
 
 const backdropClickHandler = () => {
-    toggleMovieModal();
+  toggleMovieModal();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
-confirmAddMovieButton.addEventListener('click', addMovieHandler);
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
 
 /* 
-    .trim() serve para remover os espaços em branco no início e no fim de uma string.
-
-    Ao invés de usarmos parseInt() para converter uma string em um número inteiro, podemos usar o "+" no lugar. (linhas 36 e 37) 
+    Ao criarmos uma matriz de nome "movies", desejamos adicionar a cada índice um fime (objeto). Mas vale ressaltar que não o tornará visível
+        na tela, pois o intuito será para o gerenciamento de dados em JS e não apenas como elementos HTML. 
+    E trabalhar com dados dessa forma não há a necessidade de trabalharmos com DOM (nesse caso).
 
 CONTEXTUALIZANDO O CÓDIGO:
-    Neste commit, Quando o botão de confirmação é clicado, a função addMovieHandler é chamada. 
-        Esta função recupera os valores dos campos de entrada do usuário. Se algum dos valores não for válido 
-        (por exemplo, se estiver vazio ou se a classificação não estiver entre 1 e 5), uma mensagem de alerta será exibida e 
-        a função será interrompida. Se todos os valores forem válidos, a função continuará a execução. 
-        No entanto, atualmente, mesmo que os valores sejam válidos, a função não faz nada além disso.
+    Neste commit, quando o botão de confirmação é clicado,a função addMovieHandler é chamada.
+        Esta função além de validar os inputs (commit anterior), armazena-nos em um objeto, que por sua vez, se torna um índice em um array.
+        E por fim, criamos uma função com o intuito de "limpar" os dados inseridos em cada imput.
 
-    - Adicionado o botão de confirmação: Foi adicionada uma nova variável chamada confirmAddMovieButton, 
-        que seleciona o botão de confirmação no modal de adicionar filme. (linha 10);
+    - Criação da lista de filmes: Uma nova variável chamada movies foi adicionada, 
+        que é uma lista vazia onde os filmes adicionados pelo usuário serão armazenados. (linha 14);
+
+    - Função para limpar o modal de filme: Uma nova função chamada clearMovieModal foi criada. 
+        Esta função percorre todos os campos de entrada do usuário e limpa seus valores. (linhas 31 a 34);
+
+    - Adição de um novo filme à lista de filmes: Dentro da função addMovieHandler, 
+        um novo objeto newMovie é criado com os valores dos campos de entrada do usuário. 
+        Este objeto é então adicionado à lista de filmes. (linhas 52 a 58);
+
+    - Limpeza do modal após a clicagem no botão de cancelar modal. (linha 33);
     
-    - Adicionado os campos de entrada do usuário: 
-        Foi adicionada uma nova variável chamada userInputs, que seleciona todos os campos de entrada no modal de adicionar filme. (linha 11);
-
-    - Função para adicionar um filme: Foi criada uma nova função chamada addMovieHandler. 
-        Esta função recupera os valores dos campos de entrada do usuário e verifica se eles são válidos. 
-        Se algum dos valores não for válido (por exemplo, se estiver vazio ou se a classificação não estiver entre 1 e 5), 
-        uma mensagem de alerta será exibida e a função será interrompida. 
-        Se todos os valores forem válidos, a função continuará a execução. (linhas 27 a 41 e 50);
-
-    - Manipulador de clique no botão de confirmação: Foi adicionado um novo ouvinte de evento para o botão de confirmação. 
-        Quando o botão de confirmação é clicado, a função addMovieHandler é chamada. (linha 50);
-
-OBSERVAÇÃO:
-    Com finalidade de padronização, adicionamos a palavra ...Handler às funções que apenas são chamadas através de um ouvinte de evento.
+    - Limpeza do modal após a adição de um filme: Após a adição de um filme à lista, a função clearMovieModal é chamada para limpar os 
+        campos de entrada. (linha 61);
 */
