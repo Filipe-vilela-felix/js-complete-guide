de1254c1d65f9f865110bd64ad0ecb1da336b02c
Filipe Vilela <fvf0092000@gmail.com>
2023-10-26 13:42:28 -0300

    section 8: transforming Data with map()

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjustedPrices: price * (1 - tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjustedPrices: price * (1 - tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
+
 
 /* 
-  O método forEach() é uma maneira de iterar (ou percorrer) todos os elementos de um array. Ele executa uma função para cada elemento do array. 
+  O método map() é usado para criar um novo array a partir de um array existente, aplicando uma função a cada um dos seus elementos.s
 
-  Recebe até tres argumentos:
-    O primeiro argumento é o valor que está olhando no momento.
-    O segundo argumento é o ídice que deseja ser nomeado.
-    O terceiro argumento é o array completo (opcional).
+CONTEXTUALIZANDO O CÓDIGO:
+  O método .map() está sendo usado para criar um novo array chamado taxAdjustedPrices a partir do array prices. 
+  A função passada para o .map() pega cada price do array prices, 
+    calcula o preço ajustado pelo imposto e retorna um objeto com o índice do preço no array original (idx) e o preço ajustado (taxAdjustedPrices). 
 */
