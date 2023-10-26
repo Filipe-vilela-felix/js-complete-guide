3e61cc20b101db48bcbbd83ea5baa456fd48e99e
Filipe Vilela <fvf0092000@gmail.com>
2023-10-26 14:51:42 -0300

    section 8: the important reduce() method

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const filteredArray = prices.filter((price, idx, prices) => {
-  return price < 6;
-});
-console.log(filteredArray);
+// let sum = 0;
+// prices.forEach((price) => {
+//   sum += price
+// })
+
+// console.log(sum);
+
+let sum = prices.reduce((prevValue, curValue, idx, prices) => prevValue + curValue, 0);
+console.log(sum)
 
 
 /* 
-  O método .filter() é uma função JavaScript que permite filtrar elementos de um array. 
-  Ele cria um novo array com todos os elementos que passam em um teste implementado por uma função fornecida.
+  Esse método é uma função que executa uma função de redução (fornecida por você) em cada elemento do array, 
+    resultando em um único valor de retorno.
 */
