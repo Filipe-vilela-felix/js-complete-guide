e2cc6dc11e71a8db2f0eb9b7facddf0ed55c2300
Filipe Vilela <fvf0092000@gmail.com>
2023-10-26 17:00:26 -0300

    section 8: destructuring array

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-const nameFragments = ['Max', 'Schwarz'];
-const name = nameFragments.join(' ');
-console.log(name);
+const nameData = ['Max', 'Schwarz', 'Mr', 30];
+// const firstName = nameData[0];
+// const lastName = nameData[1];
 
-const copiedNameFragments = [...nameFragments];
-nameFragments.push('Mr');
-
-console.log(nameFragments, copiedNameFragments) // Note que a string 'Mr' não estará presente na contante que contém  o spread.
-
-console.log('---------------------');
-
-const prices = [1.99, 5.99, 3.99, 6.59];
-
-console.log(Math.min(...prices));
-console.log(Math.min(prices)); // Note que sem o spread, o resultado é NaN.
-
-console.log('---------------------');
-
-const persons = [{name: 'Max', age: 30}, {name: 'Manuel', age: 31}];
-const copiedPersons = [...persons.map(person => ({
-  name: person.name, 
-  age: person.age
-}))];
-
-persons.push({name: 'Anna', age: 29});  
-persons[0].age = '31'; // Para que essa linha de código não altere a contante que tem o spread, usamos o .map() nela. (linhas 20 a 23);
-
-console.log(persons, copiedPersons);
+const [firstName, lastName, ...otherInformation] = nameData;  // Usamos o operador spread para que ele possa "copiar" o resto dos índices no array.
+console.log(firstName, lastName, ...otherInformation);  // Os elementos em ...otherInformation serão apresentados apenas como elementos.
+console.log(firstName, lastName, otherInformation); // Os elementos em otherInformation serão apresentados como array.
 
 /* 
-  Spread Operator (Operador de Propagação):
-    O operador spread extrai todos os elementos de um array e os fornece como uma lista independente de elementos.
-    O operador spread (...) no JavaScript é como uma ferramenta que pega coisas agrupadas, como uma lista de itens ou letras em uma palavra, 
-      e as espalha para que possam ser usadas individualmente. 
-    Ele é útil quando você quer usar itens de uma lista um por um, ou quando quer combinar coisas de diferentes grupos em um só.
+  A desestruturação do array é um recurso importante se desejarmos dividir um array em variáveis ou constantes que podemos usar em todo o código.
 */
