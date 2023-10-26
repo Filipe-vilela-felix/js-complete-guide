046b92c8a11e8e719b29155c7e639dac6270f009
Filipe Vilela <fvf0092000@gmail.com>
2023-10-26 03:40:46 -0300

    section 8: alternative to for loops: the forEach() method

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
 
-console.log(testResults.includes(10.99));
-console.log(testResults.indexOf(10.99) !== -1);
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjustedPrices: price * (1 - tax) };
+  taxAdjustedPrices.push(priceObj);
+});
+
+console.log(taxAdjustedPrices);
 
 /* 
-  O includes() é um método útil para descobrir se um elemento faz parte de um array ou não, retornando verdadeirou ou falso.
-  Este método é uma ótimoa opção se não estivermos interessados no índice e também no valor, 
-    mas apenas quisermos saber se ele faz parte da matriz ou não. (linha 3);
+  O método forEach() é uma maneira de iterar (ou percorrer) todos os elementos de um array. Ele executa uma função para cada elemento do array. 
 
-  É importante ter em mente que o índice retornará -1 se algo não for encontrado.
-  Portanto, isso é essencialmente o mesmo que executar o índice e compará-lo com não ser igual a -1. (linha 4);
+  Recebe até tres argumentos:
+    O primeiro argumento é o valor que está olhando no momento.
+    O segundo argumento é o ídice que deseja ser nomeado.
+    O terceiro argumento é o array completo (opcional).
 */
