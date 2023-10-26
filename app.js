2a36dab8d5f9c8b7691a0cda9e9b2db5c150d8c7
Filipe Vilela <fvf0092000@gmail.com>
2023-10-26 01:39:07 -0300

    section 8: finding stuff: find() and findIndex()

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-
-testResults.push(5.91);
-
-console.log(testResults.indexOf(1.5));  // Identificar o índice do número presente no array. Contudo, ele irá mostrar o valor da esquerda pra direita que aparecer.
-console.log(testResults.lastIndexOf(1.5));  // Identificar o índice do número presente no array. Contudo, ele irá mostrar o valor da direita pra esquerda que aparecer.
-
 const personData = [{name: 'Max'}, {name: 'Manuel'}];
 console.log(personData.indexOf({name: 'Manuel'}));  // Gera um erro estranho de resultado -1.
 
+const manuel = personData.find((person, idx, persons) => {
+  return person.name === 'Manuel';
+});
+
+console.log(manuel);
+
+const manuelIndex = personData.findIndex((person, idx, persons) => {
+  return person.name === 'Manuel';
+});
+
+console.log(manuelIndex);
+
 /* 
   O indexOf() ou lastIndexOf() não funciona como deveria em valores de referência, como por exemplo em objetos. (linhas 8 e 9);
+  E por esse motivo, usamos o método find(). São métodos de array em JavaScript que são usados para encontrar um elemento em um array.
+
+  O método find() retorna o primeiro elemento do array que satisfaz a função de teste fornecida. Caso contrário, ele retorna undefined.
+  O método findIndex(), por outro lado, retorna o índice do primeiro elemento do array que satisfaz a função de teste fornecida. 
+    Caso contrário, ele retorna -1.
+
+  Em ambos os métodos necessitam de uma função de seta.
+    O primeiro argumento sempre será um primeiro objeto desse array.
+    O segundo argumento sempre será o índice daquele único elemento.
+    O terceiro argumento sempre será o array completo
+
+CONTEXTUALIZANDO O CÓDIGO:
+  Usamos o método find() para encontrar a pessoa cujo nome é ‘Manuel’ no array personData. 
+  A função de teste que passamos para o método find() verifica se o nome da pessoa é ‘Manuel’. 
+  O resultado dessa operação é armazenado na variável manuel e então impresso no console.
+
+  Usamos o método findIndex() para encontrar o índice da pessoa cujo nome é ‘Manuel’ no array personData. 
+  A função de teste que você passou para o método findIndex() é a mesma que usamos com o método find(). 
+  O resultado dessa operação é armazenado na variável manuelIndex e então impresso no console.
 */
