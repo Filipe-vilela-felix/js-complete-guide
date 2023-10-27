4607a82e638674900cc32bb6b24a167cc8afeb70
Filipe Vilela <fvf0092000@gmail.com>
2023-10-27 11:52:32 -0300

    section 8: objects - recap

---
+++ assets/scripts/objects.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    greet: function() {     // Um método é simplesmente uma função atribuida a uma propriedade 
+        alert('Hi there!');
+    }       
+};
+
+person.greet();    // Podemos interagir com o umbjeto através de uma notação de ponto.
