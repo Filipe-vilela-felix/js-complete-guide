c20fe77bdd0381cc80e45b53b36dc360fc960bc8
Filipe Vilela <fvf0092000@gmail.com>
2023-10-27 16:24:01 -0300

    section 9: 'demo app' and shorthand property syntax

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,22 +1,37 @@
-const person = {
-    'first name': 'Max',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    greet: function() {     
-        alert('Hi there!');
-    },       
-    1.5: 'hello'
-};
-console.log(person);
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
-console.log(person[1.5]);
+const movies = [];
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;   // .value indica que não estou interessado no DOM completo, mas somente no valor dele.
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    // Adicionando validações:
+    if (
+        title.trim() === '' || 
+        extraName.trim() === '' || 
+        extraValue === ''
+    ) {
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title,
+            [extraName]: extraValue // Os colchetes são necessários quando queremos usar uma variável para definir o nome de uma propriedade de um objeto.
+        },
+        id: Math.random()
+    };
+
+    movies.push(newMovie);
+    console.log(newMovie);
+}
+
+addMovieButton.addEventListener('click', addMovieHandler);
 
 /* 
-    Existem dois outros tipos de valores que podemos usar para os nomes de porpriedades: números e símbolos.
-
-    Números: Podemos usar números como chaves. E para identifica-los ou chama-los usamos colchetes. (linhas )
-        Obs: Tb é possível inseir aspas aoc chama-lo. Ex: console.log(person.['1.5']);
-
-    Como as propriedades são ordenadas em um objeto?
-        Assita a metade final da video aula kkkkk.
+CONTEXTUALIZANDO O CÓDIGO:
+    Adicionando o botão 'ADD MOVIE'.
 */
