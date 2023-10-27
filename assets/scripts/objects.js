9bb663a8091654634be7be4787cf578b56bb7575
Filipe Vilela <fvf0092000@gmail.com>
2023-10-27 15:39:50 -0300

    section 9: property types and property order

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,27 +1,22 @@
-// const 'first name' = 'Max';  É gerado um erro por conta do nome da constante, ao contrário da linha 3.
 const person = {
     'first name': 'Max',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
     greet: function() {     
         alert('Hi there!');
-    }       
+    },       
+    1.5: 'hello'
 };
 console.log(person);
 
-console.log(person['first name']);
-
-const movieList = document.getElementById('movie-list');
-// movieList.style.backgroundColor = 'red';
-movieList.style['background-color'] = 'red';
-movieList.style['display'] = 'block';
+console.log(person[1.5]);
 
 /* 
-    Tudo o que podemos usar em uma variável/constate, pode ser usado em uma chave. 
-        Mas nem tudo o que podemos usar como um nome de chave, pode ser usado como uma variável/constante. (linhas 1 e 4);
+    Existem dois outros tipos de valores que podemos usar para os nomes de porpriedades: números e símbolos.
 
-    Para darmos um nome separado a uma chave ou qualquer outro tipo de nome basta o incluirmos em aspas. (linha 3);
-    Agora, chamar essa propriedade é possível através de uma notação de colchetes. (linha 12);
-        Vale ressaltar que a notação de colchetes com aspas também está presente na alteração de um elemento através do DOM. (linha 3);
-        Tal característica nos permite alterar inlcusive o nome da propriedade do elemento DOM. (linha 3);
+    Números: Podemos usar números como chaves. E para identifica-los ou chama-los usamos colchetes. (linhas )
+        Obs: Tb é possível inseir aspas aoc chama-lo. Ex: console.log(person.['1.5']);
+
+    Como as propriedades são ordenadas em um objeto?
+        Assita a metade final da video aula kkkkk.
 */
