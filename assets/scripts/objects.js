119e8512262289c4a42c496252ec544dd81b937b
Filipe Vilela <fvf0092000@gmail.com>
2023-10-27 15:16:17 -0300

    section 9: special key names and square bracket property access

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,21 +1,27 @@
+// const 'first name' = 'Max';  É gerado um erro por conta do nome da constante, ao contrário da linha 3.
 const person = {
-    name: 'Max',
+    'first name': 'Max',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    greet: function() {     // Um método é simplesmente uma função atribuida a uma propriedade 
+    greet: function() {     
         alert('Hi there!');
     }       
 };
 console.log(person);
 
-person.isAmdmin = true;
-delete person.age;
+console.log(person['first name']);
 
-console.log(person);
+const movieList = document.getElementById('movie-list');
+// movieList.style.backgroundColor = 'red';
+movieList.style['background-color'] = 'red';
+movieList.style['display'] = 'block';
 
 /* 
-    Com a notação de pontos podemos adicionar propriedades em um objeto e atribuir valores a elas. 
-        Não importando se ela fazia parte do objeto ou não. (linha 11);
+    Tudo o que podemos usar em uma variável/constate, pode ser usado em uma chave. 
+        Mas nem tudo o que podemos usar como um nome de chave, pode ser usado como uma variável/constante. (linhas 1 e 4);
 
-    Podemos tb deletar propriedades de objetos. (linha 12);
+    Para darmos um nome separado a uma chave ou qualquer outro tipo de nome basta o incluirmos em aspas. (linha 3);
+    Agora, chamar essa propriedade é possível através de uma notação de colchetes. (linha 12);
+        Vale ressaltar que a notação de colchetes com aspas também está presente na alteração de um elemento através do DOM. (linha 3);
+        Tal característica nos permite alterar inlcusive o nome da propriedade do elemento DOM. (linha 3);
 */
