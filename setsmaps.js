66107fc51c12d8be13df68b20a9d82190dd3ff25
Filipe Vilela <fvf0092000@gmail.com>
2023-10-27 10:56:17 -0300

    section 8: working with Maps

--- setsmaps.js
+++ setsmaps.js
@@ -1,29 +1,54 @@
-const ids = new Set(['Hi', 'from', 'set!']);
-console.log('---------------');
-console.log(ids[0]);    // O resultado será undefined.
-console.log('---------------');
-console.log(ids.has('from'));    // Para verificar ser o valor 1 está no conjunto, ou seja, retornar true ou false.
-console.log('---------------');
-console.log(ids.add(2));    // O valor 2 não será adicionado no conjunto como deveria, pois em um conjunto, um valor é garantido como único.
+const person1 = {name: 'Max'};
+const person2 = {name: 'Manuel'};
 
-console.log('---------------');
-// ids.entries() é um método que podemos executar e retornar um iterável, podendo user-lo em loops. Veja:
-for (const entry of ids.entries()) {
-    console.log(entry[1]);  // É necessário informar o índice para que o retorno de todos os elementos funcione.
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+console.log('---------------------');
+
+// for (const entry of personData.entries())  Como cada entry é uma estrutura de chave e valor, podemos usar a desestruturação de array. (linha 14); 
+for (const [key, value] of personData.entries()) {
+    console.log(key, value);
 }
 
-console.log('---------------');
-ids.delete('Hi');   // Deletando o elemento "Hi".
-console.log(ids);
+console.log('---------------------');
 
-console.log('---------------');
-// Faz necessário saber se o elemento a ser deletador existe, e para isso:
-if (ids.has(2)) {
-    ids.delete(2)
+for (const key of personData.keys()) {
+    console.log(key);
 }
-console.log(ids);
+
+console.log('---------------------');
+
+for (const value of personData.values()) {
+    console.log(value);
+}
+
+console.log('---------------------');
+
+console.log(personData.size)
 
 /*
-    Os sets (conjuntos) são uma estrutura de daods que ajudam a gerenciar valores únicos. E em alguns casos, podem ser úteis.
-    São ótimos quando se quer exclusividade entre os dados.
+    A vantange do Map() é que podemos usar um objeto como uma chave e anexar informações extras nele, sem afetar o objeto principal.
+    Ou seja, o Map() usa uma estrutura de chave e valor. (linha 4);
+
+    .set() é usado para adicionar um novo elemento ou atualizar um elemento existente no Map. (linha 6);
+    
+    .get() serve apenas para obter um valor por chave, inserindo a chave desejada ou o objeto usado no Map(). (linha 9);
+
+    Podemos enviar todas as informações para um mapa de três maneiras diferentes (sempre usando um for loop):
+        1ª) Estrutura de chave e valor. (linhas 13 a 16);
+        2ª) Apenas com chaves. (linhas 20 a 22);
+        3ª) Apenas com valores. (linhas 26 a 28);
+
+    Algumas propriedades extras, como a .size. (linha 32);
+
+OBSERVAÇÃO:
+    Essa estrutura pode evitar o incômodo de ter que, por exemplo, adicionar um ID a cada objeto criado, pois o próprio objeto pode ser a chave.
+    Quando criamos uma estrutua Map() com o new Map(), podemos passar uma array de arrays, onde cada array interno é uma estrutura de chave e valor.
+    Contudo, quando usamos o método .set(), estamos adicionando pares de chave e valor individualmente ao Map. 
+        Portanto, não precisa de um array de arrays.
 */
