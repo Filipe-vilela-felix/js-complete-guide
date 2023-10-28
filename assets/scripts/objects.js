1e7852bedcace7c98840873565f4630d3db2e088
Filipe Vilela <fvf0092000@gmail.com>
2023-10-28 01:00:59 -0300

    section 9: understanding 'chaining' (property and method chaining)

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -49,7 +49,7 @@ const addMovieHandler = () => {
             title,
             [extraName]: extraValue
         },
-        id: Math.random()
+        id: Math.random().toString()    // Encadeando o método toString() no resultado de matemática.
     };
 
     movies.push(newMovie);
@@ -65,11 +65,5 @@ addMovieButton.addEventListener('click', addMovieHandler);
 searchBtn.addEventListener('click', searchMovieHandler);
 
 /* 
-CONTEXTUALIZANDO O CÓDIGO:
-    - A função searchMovieHandler é uma nova função que obtém o valor do elemento ‘filter-title’ 
-        e passa esse valor para a função renderMovies como o termo de filtro. Isso permite que você filtre os filmes por título. (linhas 59 a 62);
-    
-    - Adicionamos uma funcionalidade de filtro à função renderMovies. Agora a função aceita um argumento opcional chamado filter. 
-        Se nenhum filtro for fornecido, a função irá renderizar todos os filmes na lista de filmes. 
-        Se um filtro for fornecido, a função irá renderizar apenas os filmes cujo título inclui o termo de filtro. (linhas 6 e 16 a 20);
+    Para melhor entendimento sobre encadeamento, reassista a aula.
 */
