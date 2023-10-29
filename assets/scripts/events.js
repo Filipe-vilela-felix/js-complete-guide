34620cc029decb7b2a7c68330624c68d5cba3558
Filipe Vilela <fvf0092000@gmail.com>
2023-10-29 14:54:16 -0300

    section 13: diferent was of listening to events

---
+++ assets/scripts/events.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('button');
+
+// button.onclick = function() {
+//     alert('Hello there!');
+// }
+
+// ----------------------------------
+
+// const buttonClickHandler = () => {
+//     alert('Button was clicked!');
+// }
+
+// const anotherButtonClickHandler = () => {
+//     console.log('This was clicked!');
+// }
+
+// button.onclick = buttonClickHandler;     // Essa linha acabará sendo substituida pela linha de baixo.
+// button.onclick = anotherButtonClickHandler;
+
+// -----------------------------------
+
+button.addEventListener()   // Mais recomendado do que os das linhas comentadas antarios.
