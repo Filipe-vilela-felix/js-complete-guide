d604158e38970072958d6810c739e115e5fce1d5
Filipe Vilela <fvf0092000@gmail.com>
2023-10-29 16:45:48 -0300

    section 14: event propagation and 'stopPropagations()'

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,6 +5,20 @@ form.addEventListener('submit', event => {
     console.log(event);
 });
 
+const div = document.querySelector('div');
+const button = document.querySelector('button');
+
+div.addEventListener('mouseenter', event => {
+    console.log('CLICKED DIV');
+    console.log(event);
+});
+
+button.addEventListener('mouseenter', event => {
+    event.stopPropagation();
+    console.log('CLICKED BUTTON');
+    console.log(event);
+});
+
 /* 
     Assistir aula teórica (312).
 */
