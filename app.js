013ce950a5c97062818e8c1fc90f5a8ae8cb0675
Filipe Vilela <fvf0092000@gmail.com>
2023-10-31 15:33:41 -0300

    section 15: clousures

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let multiplier = 1.1;
+
 function createTaxCalculator(tax) {
     function calculateTax(amount) {
+        console.log(multiplier);
         return amount * tax;
     }
 
@@ -8,6 +11,8 @@ function createTaxCalculator(tax) {
 
 const calculateValAmount = createTaxCalculator(0.19);   // Esse é o valor de 'tax' na função createTaxCalculator().
 const CalculateIncomeTaxAmount = createTaxCalculator(0.25);     // Esse é o valor de 'tax' na função createTaxCalculator().
+
+multiplier = 1.2;   // NO console do navegador, o valor é alterado para 1.2 pois a função createTaxCalculator() ainda n foi chamada.
 
 console.log(calculateValAmount(100));   // Esse é o valor de 'amount' na função calculateTax().
 console.log(calculateValAmount(200));   // Esse é o valor de 'amount' na função calculateTax().
