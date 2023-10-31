21b83203fce587f10ea1d7c3d5df5837a69481ef
Filipe Vilela <fvf0092000@gmail.com>
2023-10-31 15:48:02 -0300

    section 15: closures in practice

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-let multiplier = 1.1;
+let userName = 'Max';
 
-function createTaxCalculator(tax) {
-    function calculateTax(amount) {
-        console.log(multiplier);
-        return amount * tax;
-    }
-
-    return calculateTax;
+function greetUser() {
+    let name = 'Anna';
+    console.log('Hi' + name);
 }
 
-const calculateValAmount = createTaxCalculator(0.19);   // Esse é o valor de 'tax' na função createTaxCalculator().
-const CalculateIncomeTaxAmount = createTaxCalculator(0.25);     // Esse é o valor de 'tax' na função createTaxCalculator().
+let name = 'Maximilian'
 
-multiplier = 1.2;   // NO console do navegador, o valor é alterado para 1.2 pois a função createTaxCalculator() ainda n foi chamada.
+userName = 'Manuel';
 
-console.log(calculateValAmount(100));   // Esse é o valor de 'amount' na função calculateTax().
-console.log(calculateValAmount(200));   // Esse é o valor de 'amount' na função calculateTax().
+greetUser();
