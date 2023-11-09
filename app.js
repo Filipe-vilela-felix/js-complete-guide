aa950c7861688944de89a00044d72d8f96282be6
Filipe Vilela <fvf0092000@gmail.com>
2023-11-09 01:14:56 -0300

    section 26: understanding iterators

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-// Library land
-const uid = Symbol('uid');
-console.log(uid);
+// // Library land
+// const uid = Symbol('uid');
+// console.log(uid);
 
-const user = {
-    // id: 'pl',
-    [uid]: 'p1',
-    name: 'Max',
-    age: 30,
-    [Symbol.toStringTag]: 'User'; 
+// const user = {
+//     // id: 'pl',
+//     [uid]: 'p1',
+//     name: 'Max',
+//     age: 30,
+//     [Symbol.toStringTag]: 'User'
+// };
+
+// user[uid] = 'p3';
+
+// // app land => Using the library
+
+// user.id = 'p2'; // this should not be possible!
+
+// console.log(user[Symbol('uid')]);
+// console.log(Symbol('uid') === Symbol('uid'));
+// console.log(user.toStringTag());
+
+const company = {
+    curEmployee: 0,
+    employees: ['Max', 'Manu', 'Anna'],
+    next() {
+        if (this.curEmployee >= this.employees.length) {
+            return { value: this.curEmployee, done: true };
+        }
+        const returnValue = { value: this.employees[this.curEmployee], done: false };
+        this.curEmployee++;
+        return returnValue;
+    }
 };
 
-user[uid] = 'p3';
+let employee = company.next();
 
-// app land => Using the library
-
-user.id = 'p2'; // this should not be possible!
-
-console.log(user[Symbol('uid')]);
-console.log(Symbol('uid') === Symbol('uid'));
-console.log(user.toStringTag());
+while(!employee.done) {
+    console.log(employee.value);
+    employee = company.next();
+}
