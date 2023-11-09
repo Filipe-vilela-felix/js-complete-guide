b260bada74f2552ab32a048471329554929f0820
Filipe Vilela <fvf0092000@gmail.com>
2023-11-09 19:11:45 -0300

    section 27: adding basic rest routes

--- files.js
+++ files.js
@@ -1,16 +1,17 @@
-const fs = require('fs');   // A função require() é usada para importar módulos em Node.js. Neste caso, está importando o módulo ‘fs’.
+const fs = require('fs');
 
 fs.readFile('user-data.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data.toString());
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString());
 });
 
 fs.writeFile('user-data.txt', 'username=Max', err => {
-    if (err) {
-        console.log(err); 
-    } else {
-        console.log('wrote to file!');
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Wrote to file!');
+  }
 });
