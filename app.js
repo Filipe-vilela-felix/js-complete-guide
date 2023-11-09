d6701ca19621456ed59f16ccc25718da9ed0119d
Filipe Vilela <fvf0092000@gmail.com>
2023-11-09 00:40:16 -0300

    section 26: well-known symbols

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const user = {
     // id: 'pl',
     [uid]: 'p1',
     name: 'Max',
-    age: 30
+    age: 30,
+    [Symbol.toStringTag]: 'User'; 
 };
 
 user[uid] = 'p3';
@@ -16,7 +17,5 @@ user[uid] = 'p3';
 user.id = 'p2'; // this should not be possible!
 
 console.log(user[Symbol('uid')]);
-
 console.log(Symbol('uid') === Symbol('uid'));
-
-console.log(user);
+console.log(user.toStringTag());
